9268d9a9f109fcf290f2c16e465d3bdc16075088
Eka <ekafyi@users.noreply.github.com>
2019-08-01 02:14:41 +0700

    Fix package name

---
+++ examples/site-basic-local/gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-theme-musician",
+      options: {
+        // basePath: "/" // default is `/`
+        // contentPath: "content" // default is `content`
+      },
+    },
+  ],
+}
