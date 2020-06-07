de774e9ce3ba06b38f1d36a2180b8ddadc0ce2cb
Eka <6597211+ekafyi@users.noreply.github.com>
2020-06-07 17:39:43 +0700

    replace example site with fresh install of Gatsby hello-world starter

--- example-minimal/gatsby-config.js
+++ example-minimal/gatsby-config.js
@@ -6,12 +6,5 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [
-    // {
-    //   resolve: "gatsby-theme-musician",
-    //   options: {
-    //     pwaName: "The Undefined",
-    //   },
-    // },
-  ],
+  plugins: [],
 }
